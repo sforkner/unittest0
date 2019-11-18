1b7dbc41b3890e5729e176df0bfe3891ccb830ca
SForkner <sforkner@juno.com>
2019-11-18 16:18:56 -0500

    Added montly_schedule to Epmloyee class and put in tests with moked responses.

--- employee.py
+++ employee.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class Employee:
     "A sample Employee Class"
 
@@ -18,3 +21,10 @@ class Employee:
 
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response!'
